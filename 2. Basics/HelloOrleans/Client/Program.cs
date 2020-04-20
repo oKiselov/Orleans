466c96d2cee0d48a714746b18dfb552f83e6660b
Oleksandr Kiselov <oleksandr.kiselov@sitecore.com>
2020-04-21 02:22:52 +0300

    3. Client Added

---
+++ 2. Basics/HelloOrleans/Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Configuration;
+
+namespace Client
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunMainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await StartClient())
+                {
+                    Console.WriteLine($"Client is initialized: {client.IsInitialized}");
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+
+        static async Task<IClusterClient> StartClient()
+        {
+            var client = new ClientBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "HelloApp";
+                })
+                .UseLocalhostClustering()
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Client connected");
+
+            return client;
+        }
+    }
+}
