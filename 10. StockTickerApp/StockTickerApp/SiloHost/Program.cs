0f859d9a76c6b4aa3df3dfe50dce8b680245784c
Oleksandr Kiselov <oleksandr.kiselov@sitecore.com>
2020-05-11 02:18:14 +0300

    new App created - StockTickerApp

---
+++ 10. StockTickerApp/StockTickerApp/SiloHost/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+namespace SiloHost
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunSilo().Result;
+        }
+
+        private static async Task<int> RunSilo()
+        {
+            try
+            {
+                var host = await StartSilo();
+
+                Console.WriteLine("Press enter to terminate Silo...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return -1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var silo = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "stock-stream-cluster";
+                    options.ServiceId = "StockTickerApp";
+                })
+                .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(StocksStreamingGrain).Assembly).WithReferences())
+                .ConfigureLogging(logging =>
+                    logging
+                        .AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddConsole())
+                .Build();
+
+            await silo.StartAsync();
+
+            return silo;
+        }
+    }
+}
---
+++ 10. StockTickerApp/StockTickerApp/StocksStreamSubscriptionClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grains;
+using Orleans;
+using Orleans.Configuration;
+
+namespace StocksStreamSubscriptionClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = InitClient().Result;
+        }
+
+        private static async Task<IClusterClient> InitClient()
+        {
+            var client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "stock-stream-cluster";
+                    options.ServiceId = "StockTickerApp";
+                })
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(StocksStreamingGrain).Assembly).WithReferences())
+                .Build();
+
+            await client.Connect();
+
+            Console.WriteLine("Client successfully connected to SiloHost!!!");
+
+            return client;
+        }
+    }
+}
